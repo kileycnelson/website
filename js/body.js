91f31e0449f2d7f72a2d6d47e7d669ad3b799492
kileycnelson <43419459+kileycnelson@users.noreply.github.com>
2018-12-22 14:33:21 -0600

    Add files via upload

---
+++ js/body.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	
+
+$(".uterus").mouseenter(function(){
+		$(".definitionbox").html('<p class="titlesorta"> Uterus </p> <br> <p class="textsorta"> The uterus is a hollow, pear-shaped organ that is the home to a developing fetus. The uterus is divided into two parts: the cervix, which is the lower part that opens into the vagina, and the main body of the uterus, called the corpus. The corpus can easily expand to hold a developing baby. A channel through the cervix allows sperm to enter and menstrual blood to exit. </p>');
+	});
+	
+	$(".illustrationtwo").mouseenter(function(){
+		$(".definitionbox").html();
+	});
+
+
+$(".fallopiantubesdef").mouseenter(function(){
+		$(".definitionbox").html('<p class="titlesorta"> Fallopian Tubes </p> <br> <p class="textsorta"> The uterine tubes, also known as the fallopian tubes, are the female structures that transport the eggs from the ovary to the uterus each month.The egg moves through one of your fallopian tubes for a few days, waiting for sperm to come fertilize it. In the presence of sperm and fertilization, the fallopian tubes transport the fertilized egg to the uterus for implantation.  </p>');
+	});
+	
+	$(".illustrationtwo").mouseenter(function(){
+		$(".definitionbox").html("   ");
+	});
+
+
+$(".ovarydef").mouseenter(function(){
+		$(".definitionbox").html('<p class="titlesorta"> Ovaries </p> <br> <p class="textsorta"> You have 2 ovaries, and each one holds a bunch of eggs. Eggs are transported from the ovaries to the uterus via the fallopian tubes. Estrogen and progesterone are also manufactured in the ovaries. </p>');
+	});
+	
+	$(".illustrationtwo").mouseenter(function(){
+		$(".definitionbox").html("   ");
+	});
+
+$(".cervixdef").mouseenter(function(){
+		$(".definitionbox").html('<p class="titlesorta"> Cervix </p> <br> <p class="textsorta"> The cervix is the lower portion of the uterus. It is approximately two inches long, and its tubular in shape. It connects the uterus to the vagina and widens during childbirth to allow for the passage of the baby. It also allows for the passage of menstrual fluid from the uterus, and sperm needs to travel through the cervix in order to reach the uterus. </p>');
+	});
+	
+	$(".illustrationtwo").mouseenter(function(){
+		$(".definitionbox").html("   ");
+	});
+
+
+$(".vaginadef").mouseenter(function(){
+		$(".definitionbox").html('<p class="titlesorta"> Vagina </p> <br> <p class="textsorta">  The vagina is the passage that connects the uterus to the outside of her body.  The vagina is where your monthly menstrual fluid leaves your body, as well as where a baby leaves the mothers body at the end of pregnancy. It is also where the penis enters the female body during sex and where youd place a tampon during your period.  </p>');
+	});
+	
+	$(".illustrationtwo").mouseenter(function(){
+		$(".definitionbox").html("   ");
+	});
+
+
+	});
---
+++ js/script.js
@@ -0,0 +1,168 @@
+$(document).ready(function() {
+
+/* STDS*/
+/* Chlamydia*/
+$(".Chlamydia").mouseenter(function(){
+		$(".description").html('<p class="titlesorta"> Chlamydia </p> <br> <p class="textsorta"> Chlamydia is a SUPER common bacterial infection that you can get from sexual contact with another person. Chlamydia is spread through vaginal, anal, and oral sex. The infection is carried in semen and vaginal fluids. It’s one of the most common STDs, and most people who have chlamydia don’t show any symptoms. But don’t worry, Chlamydia can be easily cleared up with antibiotics.  </p>');
+	});
+	
+$(".list").mouseenter(function(){
+		$(".description").html("");
+	});
+
+/* Herpes*/
+$(".Herpes").mouseenter(function(){
+		$(".description").html('<p class="titlesorta"> Herpes </p> <br> <p class="textsorta"> Herpes is a common virus that causes sores on your genitals and/or mouth. Herpes is spread from skin-to-skin contact with infected areas, often during vaginal sex, oral sex, anal sex, and kissing. Many people with herpes don’t notice the sores or mistake them for something else, so they might not know they’re infected. Herpes can be annoying and painful, but it usually doesn’t lead to serious health problems. </p>');
+	});
+	
+$(".two").mouseenter(function(){
+		$(".description").html("  ");
+	});
+
+
+/* HIV & AIDS*/
+$(".HIVAIDS").mouseenter(function(){
+		$(".description").html('<p class="titlesorta"> HIV & AIDS </p> <br> <p class="textsorta"> HIV (Human Immunodeficiency Virus) is the virus that causes AIDS. It damages your immune system, making it easier for you to get sick. HIV is spread during sex, but condoms can help protect you. It’s a virus that breaks down certain cells in your immune system. When HIV damages your immune system, it’s easier to get really sick and even die from infections that your body could normally fight off. Once you have HIV, the virus stays in your body for life. There’s no cure for HIV, but medication can help you stay healthy longer and lower your chances of spreading the virus to other people. </p>');
+	});
+	
+	$(".list").mouseenter(function(){
+		$(".description").html("  ");
+	});
+
+/* HPV*/
+$(".hpv").mouseenter(function(){
+		$(".description").html('<p class="titlesorta"> HPV </p> <br> <p class="textsorta"> HPV stands for human papillomavirus. It’s the most common sexually transmitted infection. HPV is usually harmless and goes away by itself, but some types can lead to cancer or genital warts. There are more than 200 types of human papillomavirus (HPV). About 40 kinds can infect your genital area as well as your mouth and throat. These kinda of HPV are spread during sexual contact. Most people who have sex get the HPV at some point in their lives.  </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".description").html("  ");
+	});
+
+/* Gonorrhea*/
+$(".Gonorrhea").mouseenter(function(){
+		$(".description").html('<p class="titlesorta"> Gonorrhea </p> <br> <p class="textsorta"> Gonorrhea is a common bacterial infection that’s easily cured with antibiotic medicine. It’s sexually transmitted, and most people with gonorrhea don’t have symptoms. Its very common for teens and people in their 20s. Gonorrhea is easily cured with antibiotics. However, if you don’t treat gonorrhea early enough, it can lead to more serious health problems in the future. That’s why STD testing is so important. The sooner you know you have gonorrhea, the faster you can get rid of it.    </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".description").html("  ");
+	});
+
+/* Hepititas*/
+$(".Hepititas").mouseenter(function(){
+		$(".description").html('<p class="titlesorta"> Hepititas </p> <br> <p class="textsorta"> Hepatitis B is an infection that can cause liver disease. It is really contagious and can be spread through sex. It can be serious and there’s no cure, but the good news is it’s easy to prevent. You can protect yourself by getting the hepatitis B vaccine and having safer sex. If you have oral, anal, and vaginal sex, use condoms and dental dams to help stop the spread of hepatitis B and other STDs.  </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".description").html("  ");
+	});
+
+
+
+
+
+/* PREVENTION*/
+
+/* IUD SECTION */
+
+	$(".iud").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> IUD </p> <br> <p class="textsorta"> An IUD is a tiny device that is put into your uterus to prevent pregnancy. IUD stands for Intrauterine Device. It is a small piece of flexible plastic shaped like a T. There are 5 different brands of IUDs that are FDA approved for use in the United States: ParaGard, Mirena, Kyleena, Liletta, and Skyla. It’s long-term, reversible, and one of the most effective birth control methods out there. </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html("  ");
+	});
+
+/* implant SECTION */
+	$(".implant").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> Implant </p> <br> <p class="textsorta"> The birth control implant (AKA Nexplanon) is a tiny, thin rod about the size of a matchstick. The implant releases hormones into your body that prevent you from getting pregnant. A nurse or doctor inserts the implant into your arm and that’s it — you’re protected from pregnancy for up to 5 years. It’s get-it-and-forget-it birth control. </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html("  ");
+	});
+
+/* shot SECTION */
+	$(".shot").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> Shot </p> <br> <p class="textsorta"> The depo shot (AKA Depo-Provera) is an injection you get once every 3 months. It’s a safe, convenient, and private birth control method that works really well if you always get it on time. The Depo shot contains the hormone progestin which stops you from getting pregnant by preventing ovulation. When theres no egg in the tube, pregnancy can’t happen. </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html("  ");
+	});
+
+/* ring SECTION */
+	$(".ring").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> Ring </p> <br> <p class="textsorta"> The birth control ring (AKA NuvaRing) is a safe, simple, and affordable birth control method that you wear inside your vagina. The small, flexible ring prevents pregnancy by releasing the hormones estrogen and progestin into your body. The ring is really effective if you always use it the right way. NuvaRing hormones stop ovulation, therefore its impossible to get pregnant.  </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html("  ");
+	});
+
+/* patch SECTION */
+	$(".patch").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> Patch </p> <br> <p class="textsorta"> The transdermal contraceptive patch is a safe, simple, and affordable birth control method that you wear on the skin of your belly, upper arm, butt, or back. The hormones are then absorbed through your skin. Put a new patch on every week for 3 weeks, and it releases hormones that prevent pregnancy. Then you get a week off before you repeat the cycle. </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html("  ");
+	});
+
+/* pill SECTION */
+	$(".pilltwo").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> Pill </p> <br> <p class="textsorta"> Birth control pills are a kind of medicine with hormones. Birth control pills come in a pack, and you take 1 pill every day. The pill is safe, affordable, and effective if you always take your pill on time. Forgetting pills, losing the pack, not refilling your prescription on time — these are the main reasons why people might get pregnant when they use the pill. Besides preventing pregnancy, the pill has lots of other health benefits, too. </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html("  ");
+	});
+
+/* condom SECTION */
+	$(".condom").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> Condom </p> <br> <p class="textsorta"> Condoms are thin, stretchy pouches that you wear on your penis during sex. Condoms stop sperm from getting into the vagina, so sperm can’t meet up with an egg and cause pregnancy. Condoms provide great protection from both pregnancy and STDs. They’re easy to use and easy to get.  </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html("  ");
+	});
+
+/* Plan B SECTION */
+	$(".PlanB").mouseenter(function(){
+		$(".descriptiontwo").html('<p class="titlesorta"> Plan B </p> <br> <p class="textsorta"> You can take an emergency contraceptive pill (AKA the morning-after pill) within 120 hours (5 days) after having unprotected sex. There are 2 types of morning-after pills. One option is a pill with ulipristal acetate. Theres only one brand, called ella. Or theres a pill with levonorgestrel. Brand names include: Plan B One step, Take Action, My Way and AfterPill. </p>');
+	});
+	
+	$(".listbox").mouseenter(function(){
+		$(".descriptiontwo").html();
+	});
+
+
+
+
+});
+
+	/* PERIODSSECTION */
+
+	$(".uterus").mouseenter(function(){
+		$(".definitionbox").html('<p class="titlesorta"> Uterus </p> <br> <p class="textsorta"> hi </p>');
+	});
+	
+	$(".illustrationtwo").mouseenter(function(){
+		$(".definitionbox").html();
+	});
+
+
+$(".fallopiantubesdef").mouseenter(function(){
+		$(".definitionbox").html('<p class="titlesorta"> Fallopian Tubes </p> <br> <p class="textsorta">  hey </p>');
+	});
+	
+	$(".illustrationtwo").mouseenter(function(){
+		$(".definitionbox").html("   ");
+	});
+
+
+
+
+
+
+
+
